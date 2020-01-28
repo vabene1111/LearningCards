e8cb9762e1d6c7e4690bd6b4ef03c05a85b93e72
vabene1111 <vabene1234@googlemail.com>
2020-01-28 10:04:53 +0100

    added uni to admin

--- Cards/admin.py
+++ Cards/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from Cards.models import *
 
+admin.site.register(University)
 admin.site.register(Course)
 admin.site.register(Question)
 admin.site.register(QuestionLog)
