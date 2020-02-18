1af6283604490a94cf12c24f4a01e3804c34bd30
vabene1111 <vabene1234@googlemail.com>
2020-02-18 22:05:20 +0100

    admin improvements

--- Cards/admin.py
+++ Cards/admin.py
@@ -2,11 +2,48 @@ from django.contrib import admin
 
 from Cards.models import *
 
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+
+    @staticmethod
+    def name(obj):
+        return obj.user.first_name + ' ' + obj.user.last_name
+
+
+class TestQuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'question', 'type', 'test_id')
+
+    @staticmethod
+    def name(obj):
+        return obj.test.user.first_name + ' ' + obj.test.user.last_name
+
+
+class QuestionLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'question', 'created_at')
+
+    @staticmethod
+    def name(obj):
+        return obj.user.first_name + ' ' + obj.user.last_name
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'text', 'question_id', 'created_at')
+
+    @staticmethod
+    def name(obj):
+        return obj.created_by.first_name + ' ' + obj.created_by.last_name
+
+    @staticmethod
+    def question_id(obj):
+        return obj.question.id
+
+
 admin.site.register(University)
 admin.site.register(Course)
 admin.site.register(Question)
-admin.site.register(QuestionLog)
+admin.site.register(QuestionLog, QuestionLogAdmin)
 admin.site.register(QuestionCache)
-admin.site.register(Comment)
-admin.site.register(Test)
-admin.site.register(TestQuestion)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(TestQuestion, TestQuestionAdmin)
--- Cards/models.py
+++ Cards/models.py
@@ -79,6 +79,9 @@ class TestQuestion(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     type = models.CharField(choices=QuestionLog.STATUS_TYPES, max_length=128, blank=True, null=True)
 
+    def __str__(self):
+        return str(self.test) + ' - ' + str(self.question)
+
 
 class Comment(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -86,3 +89,6 @@ class Comment(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.created_by) + ' - ' + str(self.text)
