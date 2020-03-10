c5a282b08bb165efa39febf880eedee3f219ff6d
vabene1111 <vabene1234@googlemail.com>
2020-03-10 20:13:48 +0100

    added course export

--- Cards/helper/course_helper.py
+++ Cards/helper/course_helper.py
@@ -4,19 +4,28 @@ from Cards.models import *
 from Cards.tables import QuestionTable
 
 
-def get_chapters(course):
+def get_chapters(course, format='table'):
     chapters = {}
 
     for ch in course.chapter_set.all():
         chapters[ch.pk] = {
             'name': ch.name,
-            'table': QuestionTable(Question.objects.filter(chapter=ch).order_by('pk').all())
         }
+        if format == 'table':
+            chapters[ch.pk]['table'] = QuestionTable(Question.objects.filter(chapter=ch).order_by('pk').all())
+        else:
+            chapters[ch.pk]['questions'] = Question.objects.filter(chapter=ch).order_by('pk').all()
 
     if Question.objects.filter(chapter=None, course=course).order_by('pk').all().count() > 0:
         chapters['No_Chapter'] = {
             'name': _("No Chapter"),
-            'table': QuestionTable(Question.objects.filter(chapter=None, course=course).order_by('pk').all())
         }
+
+        if format == 'table':
+            chapters['No_Chapter']['table'] = QuestionTable(
+                Question.objects.filter(chapter=None, course=course).order_by('pk').all())
+        else:
+            chapters['No_Chapter']['questions'] = Question.objects.filter(chapter=None, course=course).order_by(
+                'pk').all()
 
     return chapters
--- Cards/views/files.py
+++ Cards/views/files.py
@@ -13,15 +13,14 @@ def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("utf8")), result, encoding='utf8')
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
 
 
 def export_course(request, pk):
-    course = get_object_or_404(Course, pk)
+    course = get_object_or_404(Course, pk=pk)
+    chapters = course_helper.get_chapters(course, format='object')
 
-    chapters = course_helper.get_chapters(course)
-
-    return render_to_pdf('export_question_pdf.html', {'questions': chapters})
+    return render_to_pdf('export_question_pdf.html', {'course': course, 'chapters': chapters})
