a55d79f36a5108d38d830f719bed092b279dc094
vabene1111 <vabene1234@googlemail.com>
2020-03-10 19:28:07 +0100

    removed useless import

--- Cards/views/files.py
+++ Cards/views/files.py
@@ -1,14 +1,12 @@
+from io import BytesIO
+
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.template.loader import render_to_string
-from fpdf import FPDF, HTMLMixin
-
-from Cards.models import Question, Course, Chapter
-from io import BytesIO
-from django.http import HttpResponse
 from django.template.loader import get_template
-
 from xhtml2pdf import pisa
+
+from Cards.helper import course_helper
+from Cards.models import Course
 
 
 def render_to_pdf(template_src, context_dict={}):
@@ -23,9 +21,7 @@ def render_to_pdf(template_src, context_dict={}):
 
 def export_course(request, pk):
     course = get_object_or_404(Course, pk)
-    chapter = Chapter.objects.filter(course=course)
-    questions = Question.objects.filter(course=course).all()
 
-    q = {}
+    chapters = course_helper.get_chapters(course)
 
-    return render_to_pdf('export_question_pdf.html', {'questions': questions})
+    return render_to_pdf('export_question_pdf.html', {'questions': chapters})
