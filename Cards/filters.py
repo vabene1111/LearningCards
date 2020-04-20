a756c2de872ff0d2f904f5cb8a677a50d2709192
vabene1111 <vabene1234@googlemail.com>
2020-04-20 15:55:46 +0200

    more autocomplete fields

--- Cards/filters.py
+++ Cards/filters.py
@@ -1,10 +1,13 @@
 import django_filters
 
-from Cards.models import Question, Course, Chapter
+from Cards.forms import SelectWidget
+from Cards.models import Question, Course, Chapter, University
 
 
 class QuestionFilter(django_filters.FilterSet):
     question = django_filters.CharFilter(lookup_expr='icontains')
+    course = django_filters.ModelChoiceFilter(widget=SelectWidget, queryset=Course.objects.all())
+    chapter = django_filters.ModelChoiceFilter(widget=SelectWidget, queryset=Chapter.objects.all())
 
     class Meta:
         model = Question
@@ -14,6 +17,7 @@ class QuestionFilter(django_filters.FilterSet):
 class CourseFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     semester = django_filters.CharFilter(lookup_expr='icontains')
+    university = django_filters.ModelChoiceFilter(widget=SelectWidget, queryset=University.objects.all())
 
     class Meta:
         model = Course
@@ -22,7 +26,9 @@ class CourseFilter(django_filters.FilterSet):
 
 class ChapterFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
+    course = django_filters.ModelChoiceFilter(widget=SelectWidget, queryset=Course.objects.all())
 
     class Meta:
         model = Chapter
         fields = ['name', 'course']
+
