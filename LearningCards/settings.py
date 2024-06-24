dfc85d8fcd28b7554377dbf0b736927da0df39ac
vabene1111 <vabene1234@googlemail.com>
2024-06-24 15:24:59 +0200

    added protocol header

--- LearningCards/settings.py
+++ LearningCards/settings.py
@@ -122,6 +122,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
